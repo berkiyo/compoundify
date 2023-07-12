43f1ade209332fe0222a84d0d25ffd4090aa5d1e
berkiyo <berk@tekbyte.net>
2023-07-12 00:52:28 +0000

    Adding graph view

--- Compoundify/App.js
+++ Compoundify/App.js
@@ -1,6 +1,22 @@
+/**
+ * Importing
+ */
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity, Dimensions, ScrollView, SafeAreaView} from 'react-native';
 import { useState } from 'react'
+import {
+	LineChart,
+	BarChart,
+	PieChart,
+	ProgressChart,
+	ContributionGraph,
+	StackedBarChart
+  } from "react-native-chart-kit";
+
+
+
+
 
 
 
@@ -10,6 +26,35 @@ export default function App() {
 	// for keeping the state of the content
 	const [enteredGoalText, setEnteredGoalText] = useState('');
 	const [courseGoals, setCourseGoals] = useState([]);
+
+	/**
+	 * Chart related operations
+	 */
+	const screenWidth = Dimensions.get("window").width;
+	const width_proportion = '80%';
+	const height_proportion = '40%';
+
+	const data = {
+		labels: ["January", "February", "March", "April", "May", "June"],
+		datasets: [
+		  {
+			data: [20, 45, 28, 80, 99, 43],
+			strokeWidth: 2 // optional
+		  }
+		],
+		legend: ["Rainy Days"] // optional
+	  };
+
+	  const chartConfig = {
+		backgroundGradientFrom: "#eeeeee",
+		backgroundGradientTo: "#fafafa",
+		color: (opacity = 1) => `rgba(60, 60, 60, ${opacity})`,
+		strokeWidth: 2, // optional, default 3
+		barPercentage: 0.5,
+		useShadowColorFromDataset: false // optional
+	  };
+
+
 
 	// Touchable Opacity Button
 	const AppButton = ({ onPress, title }) => (
@@ -36,24 +81,36 @@ export default function App() {
 	};
 
 	return (
-		<View style={styles.appContainer}>
+		<ScrollView>
+			<SafeAreaView style={styles.appContainer}>
+				
+				<View style={styles.inputContainer}>
+					<TextInput 
+						styles={styles.customInput}
+						placeholder='Your course goal!!'
+						onChangeText={goalInputHandler} />
 
-
+					<AppButton onPress={addGoalHandler} title="Add Goal"></AppButton>
 			
-			<View style={styles.inputContainer}>
-				<TextInput 
-					styles={styles.customInput}
-					placeholder='Your course goal!!'
-					onChangeText={goalInputHandler} />
+				</View>
 
-				<AppButton onPress={addGoalHandler} title="Add Goal"></AppButton>
-		
-			</View>
+				<View style={styles.goalsContainer}>
+					{courseGoals.map((goal) => <Text key={goal} style={styles.individualGoalContainer}>{goal}</Text>)}
+				</View>
 
-			<View style={styles.goalsContainer}>
-				{courseGoals.map((goal) => <Text key={goal} style={styles.individualGoalContainer}>{goal}</Text>)}
-			</View>
-		</View>
+				<LineChart
+					data={data}
+					width={300} // you can set this to screenWidth
+					height={220}
+					chartConfig={chartConfig}
+					bezier
+					style={{
+					marginVertical: 8,
+					borderRadius: 16
+					}}
+				/>
+			</SafeAreaView>
+		</ScrollView>
 	);
 }
 
@@ -65,8 +122,9 @@ const styles = StyleSheet.create({
 	appContainer: {
 		flex: 1,
 		gap: 30,
-		padding: 60,
-		paddingHorizontal: 20,
+		padding: 30,
+		paddingVertical: 60,
+		paddingHorizontal: 30,
 	},
 
 	// the input container
@@ -77,24 +135,21 @@ const styles = StyleSheet.create({
 		justifyContent: 'space-between',
 		paddingHorizontal: 10,
 		alignItems: 'center',
-		borderRadius: 8,
+		borderRadius: 20,
 		borderBottomColor: '#cccccc',
 		
 	},
 
 	// for text input container
 	customInput: {
-		borderColor: '#000000',
-		borderWidth: 9,
+		borderWidth: 2,
+		borderColor: '#cccccc',
 		width: '80%',
 		marginRight: 8,
-		padding: 8,
-		fontSize: 30,
+		padding: 8
 	},
 
-	/**
-	 * Goal styles go here
-	 */
+	// goals container
 	goalsContainer: {
 		flex: 5,
 		padding: 20,
--- README.md
+++ README.md
@@ -1,16 +1,14 @@
 # compoundify
-📈 A beautiful compound interest calculator app for iPhone and iPad.
+📈 A beautiful compound interest calculator app for iOS and Android, written in React Native.
 
 ## features
 
-- Very fast
 - 100% offline (use locally) 
 - Ability to customise graph
 - No ads
-- Fully free
 - Open source
 - Customisable graph
 
 ## development
 
-This app is currently under rapid development.
+This app is currently under development.
